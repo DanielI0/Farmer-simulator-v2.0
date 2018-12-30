ac4db65bc1a563dae3c87f10c3655a5eb1d4420f
DanielIO <danilbodrov70@gmail.com>
2018-12-31 00:34:25 +0500

    API >= 23

--- app/src/main/java/com/example/danie/kbect/MainActivity.java
+++ app/src/main/java/com/example/danie/kbect/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.danie.kbect;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -21,8 +25,9 @@ public class MainActivity extends AppCompatActivity {
     static Modules core = new Modules();
     @Override
     protected void onPause() {
-        super.onPause();
         core.save();
+        super.onPause();
+
     }
 
     @Override
@@ -32,10 +37,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+
          handler = new Handler() {   // создание хэндлера
             @Override
             public void handleMessage(Message msg) {
